164ce1d6965b59b10562f86544ab05f2dd5dfd94
Oliver Rhyme Guibone Añasco <35908542+OliverRhyme@users.noreply.github.com>
2021-10-10 20:42:07 +0800

    Improve root privilege elevation
    This should enable calling the script without calling sudo on the whole script which breaks the `docker compose` detection for user-only installations

--- install_on_linux.sh
+++ install_on_linux.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+
 ARCHITECTURE=amd64
 if [ "$(uname -m)" = "aarch64" ]; then
   ARCHITECTURE=arm64
@@ -10,25 +11,40 @@ COMPOSE_SWITCH_VERSION="v1.0.2"
 COMPOSE_SWITCH_URL="https://github.com/docker/compose-switch/releases/download/${COMPOSE_SWITCH_VERSION}/docker-compose-linux-${ARCHITECTURE}"
 
 error=$(docker compose version 2>&1 >/dev/null)
+echo "After error"
 if [ $? -ne 0 ]; then
   echo "Docker Compose V2 is not installed"
   exit 1
 fi
 
-curl -fL $COMPOSE_SWITCH_URL -o /usr/local/bin/compose-switch
-chmod +x /usr/local/bin/compose-switch
+sh_c='sh -c'
+sudo_sh_c='sh -c'
+if [ "$user" != 'root' ]; then
+    if [ "$(command -v sudo)" ]; then
+        sudo_sh_c='sudo -E sh -c'
+    elif [ "$(command -v su)" ]; then
+        sudo_sh_c='su -c'
+    else
+        echo "Error: This installer needs the ability to run commands as root."
+        exit 1
+    fi
+fi
+
+$sudo_sh_c "curl -fL $COMPOSE_SWITCH_URL -o /usr/local/bin/compose-switch"
+$sudo_sh_c "chmod +x /usr/local/bin/compose-switch"
 
 COMPOSE=$(command -v docker-compose)
 if [ "$COMPOSE" = /usr/local/bin/docker-compose ]; then
   # This is a manual installation of docker-compose
   # so, safe for us to rename binary
-  mv /usr/local/bin/docker-compose /usr/local/bin/docker-compose-v1
+  $sudo_sh_c "mv /usr/local/bin/docker-compose /usr/local/bin/docker-compose-v1"
   COMPOSE=/usr/local/bin/docker-compose-v1
 fi
 
-ALTERNATIVES="update-alternatives"
+
+ALTERNATIVES="$sudo_sh_c update-alternatives"
 if ! command -v $ALTERNATIVES; then
-  ALTERNATIVES=alternatives
+  ALTERNATIVES="$sudo_sh_c alternatives"
 fi  
 
 echo "Configuring `docker-compose` alternatives"
